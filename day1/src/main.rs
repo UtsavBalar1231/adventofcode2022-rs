f6d11103aeca1aff0174c61a8fb2d1639adac998
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-01-22 22:25:33 +0530

    adventofcode2022: day1 part 2 complete
    Change-Id: Ia398b939b1940b61107f06f43f65c228f6df37ac
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,59 +1,11 @@
-use std::collections::BTreeMap;
 use std::fs::File;
-#[allow(unused_imports)]
-use std::io::{read_to_string, Read, Result};
-use std::usize;
+use std::io::{read_to_string, Result};
 
-#[allow(dead_code)]
 fn read_input_file() -> Result<String> {
     let mut file = File::open("input.txt")?;
     let content = read_to_string(&mut file)?;
 
     Ok(content)
-}
-
-#[allow(dead_code)]
-fn create_input_hashmap(input: &mut String) -> Result<BTreeMap<usize, Vec<usize>>> {
-    let mut list = input
-        .split("\n\n")
-        .collect::<Vec<_>>()
-        .iter_mut()
-        .map(|s| s.split("\n").collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-    // remove the last element, which is an empty String
-    if list.last().unwrap().last().unwrap().is_empty() {
-        list.pop();
-    }
-
-    let map = list
-        .iter()
-        .enumerate()
-        .map(|l| {
-            let key = l.0;
-            let value =
-                l.1.iter()
-                    .map(|s| s.split(" ").collect::<Vec<_>>())
-                    .flatten()
-                    .map(|x| x.parse::<usize>().unwrap())
-                    .collect::<Vec<_>>();
-            (key, value)
-        })
-        .collect::<BTreeMap<usize, Vec<_>>>();
-
-    Ok(map)
-}
-
-#[allow(dead_code)]
-fn create_hashmap_sum(map: BTreeMap<usize, Vec<usize>>) -> BTreeMap<usize, usize> {
-    let mut sum_map = BTreeMap::new();
-
-    for (key, value) in map {
-        let sum = value.iter().sum();
-        sum_map.insert(key, sum);
-    }
-
-    sum_map
 }
 
 fn parse_string_usize(input: &mut String) -> Result<Vec<usize>> {
@@ -93,16 +45,17 @@ fn parse_string_usize(input: &mut String) -> Result<Vec<usize>> {
 
 fn main() -> Result<()> {
     let mut input = read_input_file()?;
-    // let map = create_input_hashmap(&mut input)?;
-    // let mut map = create_hashmap_sum(map);
 
+    let calories_list = parse_string_usize(&mut input).unwrap();
+    // part one
+    println!("{:?}", calories_list.iter().last().unwrap());
+
+    // part two
     println!(
         "{:?}",
-        parse_string_usize(&mut input)
-            .unwrap()
+        &calories_list[(calories_list.len() - 3)..]
             .iter()
-            .last()
-            .unwrap()
+            .sum::<usize>()
     );
 
     Ok(())
