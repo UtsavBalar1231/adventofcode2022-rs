e5f5f7d8c4c7957613a7f5307cc14980ee4ea279
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-01-22 22:11:04 +0530

    adventofcode2022: day3 part 2 complete
    Change-Id: I0b9869a10395c920dbb1484eb1e9a5814a9b0418
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -8,7 +8,7 @@ fn read_input_file() -> Result<String> {
 }
 
 fn get_rucksacks_from(input: &mut String) -> Vec<&str> {
-    input.trim().split("\n").collect()
+    input.trim().split("\n").collect::<Vec<_>>()
 }
 
 fn get_compartments_from(rucksacks: Vec<&str>) -> Vec<(&str, &str)> {
@@ -18,21 +18,40 @@ fn get_compartments_from(rucksacks: Vec<&str>) -> Vec<(&str, &str)> {
         .collect::<Vec<_>>()
 }
 
-fn compare_compartments(compartments: Vec<(&str, &str)>) -> usize {
+fn compare_compartments_part1(compartments: Vec<(&str, &str)>) -> usize {
     let mut sum = 0;
 
-    for (x, y) in compartments {
-        for i in x.chars() {
-            if y.contains(i) {
-                if i.is_lowercase() {
-                    sum += i as usize - 97 + 1;
-                } else {
-                    sum += i as usize - 65 + 26 + 1;
-                }
-                break;
-            }
+    compartments.iter().for_each(|(x, y)| {
+        let common = x.chars().filter(|i| y.contains(*i)).next().unwrap();
+
+        if common.is_lowercase() {
+            sum += common as usize - 97 + 1;
+        } else {
+            sum += common as usize - 65 + 26 + 1;
         }
-    }
+    });
+
+    sum
+}
+
+fn compare_compartments_part2(rucksacks: Vec<&str>) -> usize {
+    let mut sum = 0;
+
+    rucksacks.chunks(3).for_each(|rucksack| {
+        let common = rucksack[0]
+            .chars()
+            .filter(|x| rucksack[1].contains(*x))
+            .filter(|x| rucksack[2].contains(*x))
+            .next()
+            .unwrap();
+
+        if common.is_lowercase() {
+            sum += common as usize - 97 + 1;
+        } else {
+            sum += common as usize - 65 + 26 + 1;
+        }
+    });
+
     sum
 }
 
@@ -40,8 +59,9 @@ fn main() -> Result<()> {
     let mut input = read_input_file()?;
 
     let rucksacks = get_rucksacks_from(&mut input);
-    let compartments = get_compartments_from(rucksacks);
-    println!("{}", compare_compartments(compartments));
+    let compartments = get_compartments_from(rucksacks.clone());
+    println!("{}", compare_compartments_part1(compartments));
+    println!("{}", compare_compartments_part2(rucksacks.clone()));
 
     Ok(())
 }
